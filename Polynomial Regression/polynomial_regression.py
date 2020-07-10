b7ed9218cf7f5297552035499956dabcdc4c7d50
Yusuf Ziya <37000658+ujubu@users.noreply.github.com>
2020-07-10 09:50:31 +0300

    Update polynomial_regression.py

--- Polynomial Regression/polynomial_regression.py
+++ Polynomial Regression/polynomial_regression.py
@@ -10,10 +10,6 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Training the Linear Regression model on the whole dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
@@ -21,14 +17,6 @@ poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
-
-# Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
@@ -47,9 +35,6 @@ plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
-# Predicting a new result with Linear Regression
-lin_reg.predict([[6.5]])
 
 # Predicting a new result with Polynomial Regression
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
